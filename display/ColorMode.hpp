81d9617c171ee08a4be6d924a808b2172d0645a1
Grigorii Kuzmin <juicefruit89@gmail.com>
2020-12-02 04:07:13 +0300

    removed for rename

--- display/ColorMode.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-enum class ColorMode : uint8_t
-{
-    R5G6B5,
-    R6G6B6
-};
--- systick/systick.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Systick
-{
-    void InitSystick();
-    void DelayMilliseconds(uint32_t milliseconds);
-}
