198232d950095915d6a2103ee9e4d86575e9e5bf
Grigorii Kuzmin <juicefruit89@gmail.com>
2020-12-22 03:59:16 +0300

    forgot noexcept specificator

--- Peripherals/Random/Random.cpp
+++ Peripherals/Random/Random.cpp
@@ -6,12 +6,12 @@
 
 namespace Peripherals
 {
-    uint32_t Random::GetUint32()
+    uint32_t Random::GetUint32() noexcept
     {
         return RandomImpl::GetUint32();
     }
 
-    uint32_t Random::GetUint32(uint32_t min, uint32_t max)
+    uint32_t Random::GetUint32(uint32_t min, uint32_t max) noexcept
     {
         return RandomImpl::GetUint32(min, max);
     }
